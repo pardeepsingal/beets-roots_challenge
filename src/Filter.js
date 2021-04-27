9172bb6a70d6c899256b265575ab07b6844c35a5
Pardeep Kumar <pardeepsingal1@gmail.com>
2021-04-27 10:00:35 +0530

    handled the keyboard scorll, view port and update the hooks

--- src/Filter.js
+++ src/Filter.js
@@ -1,146 +1,127 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useRef } from 'react';
+const Filter = (props) => {
+	const [activeRecords, setActiveRecords] = useState(0);
+	const [filteredData, setFilteredData] = useState([]);
+	const [showData, setShowData] = useState(false);
+	const [userInput, setUserInput] = useState('');
 
-export class Filter extends Component {
-	static propTypes = {
-		data: PropTypes.instanceOf(Array),
-	};
-	static defaultProperty = {
-		data: [],
-	};
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeRecords: 0,
-			filteredData: [],
-			showData: false,
-			userInput: '',
-		};
-	}
-	onChange = (e) => {
-		const { data } = this.props;
+	const listRef = useRef();
+
+	const onChange = (e) => {
+		const { data } = props;
 		const searchText = e.currentTarget.value;
 		const searchedVal = searchText.toLowerCase().trim();
 		const filteredData = data.filter((item) => {
 			return Object.keys(item).some((key) => item[key].toString().toLowerCase().includes(searchedVal));
 		});
-		this.setState({
-			activeRecords: 0,
-			filteredData,
-			showData: true,
-			userInput: searchText,
-		});
+		setActiveRecords(0);
+		setFilteredData(filteredData);
+		setShowData(true);
+		setUserInput(searchText);
 	};
 
-	onClick = (e) => {
-		const { activeRecords, filteredData } = this.state;
+	const onClick = (e) => {
 		let index = e.target.getAttribute('data-index');
 		let selectedVal = filteredData[index];
-		console.log({ selectedVal });
-		console.log(index);
-		console.log({ filteredData });
-		this.setState({
-			activeRecords: 0,
-			filteredData: [],
-			showData: false,
-			userInput: selectedVal['name'],
-		});
+		setActiveRecords(0);
+		setFilteredData([]);
+		setShowData(false);
+		setUserInput(selectedVal['name']);
 	};
 
-	onKeyDown = (e) => {
-		const { activeRecords, filteredData } = this.state;
+	const onKeyDown = (e) => {
 		if (e.keyCode === 13) {
 			let selectedVal = filteredData[activeRecords];
-			this.setState({
-				activeRecords: 0,
-				showData: false,
-				userInput: selectedVal.name,
-			});
+			setActiveRecords(0);
+			setShowData(false);
+			setUserInput(selectedVal.name);
 		} else if (e.keyCode === 38) {
 			if (activeRecords === 0) {
 				return;
 			}
+			var elHeight = 97;
+			var scrollTop = listRef.current.scrollTop;
+			var viewport = scrollTop + 300;
+			var elOffset = elHeight * activeRecords - elHeight;
+			if (elOffset < scrollTop || elOffset + elHeight > viewport) {
+				listRef.current.scrollTo(0, elOffset);
+			}
 
-			this.setState({ activeRecords: activeRecords - 1 });
+			setActiveRecords(activeRecords - 1);
 		} else if (e.keyCode === 40) {
 			if (activeRecords - 1 === filteredData.length) {
 				return;
 			}
+			var elHeight = 97;
+			var scrollTop = listRef.current.scrollTop;
+			var viewport = scrollTop + 300;
+			var elOffset = elHeight * activeRecords + elHeight;
+			if (elOffset < scrollTop || elOffset + elHeight > viewport) {
+				listRef.current.scrollTo(0, elOffset);
+			}
 
-			this.setState({ activeRecords: activeRecords + 1 });
+			setActiveRecords(activeRecords + 1);
 		}
 	};
 
-	render() {
-		const {
-			onChange,
-			onClick,
-			onKeyDown,
-			state: { activeRecords, filteredData, showData, userInput },
-		} = this;
-		let DataListView;
+	let DataListView;
+	if (showData && userInput) {
+		if (filteredData.length) {
+			DataListView = (
+				<ul className="list-group" ref={listRef}>
+					{filteredData.map((suggestion, index) => {
+						let className = 'list-group-item';
 
-		if (showData && userInput) {
-			if (filteredData.length) {
-				DataListView = (
-					<ul className="list-group">
-						{filteredData.map((suggestion, index) => {
-							let className = 'list-group-item';
+						if (index === activeRecords) {
+							className = 'list-group-item active';
+						} else {
+						}
 
-							if (index === activeRecords) {
-								className = 'list-group-item active';
-							} else {
-							}
-
-							return (
-								<li className={className} data-index={index} ref={index} onClick={onClick} key={index}>
-									<b>{suggestion.id} </b>
-									<br />
-									<i>{suggestion.name} </i>
-									<br />
-									<small>{suggestion.address + ' ' + suggestion.pincode}</small>
-								</li>
-							);
-						})}
-					</ul>
-				);
-			} else {
-				DataListView = (
-					<ul className="list-group">
-						<li className="list-group-item" key={'no-data'}>
-							No User Found
-						</li>
-					</ul>
-				);
-			}
+						return (
+							<li className={className} data-index={index} onClick={onClick} key={index} tabIndex={index}>
+								<b>{suggestion.id} </b>
+								<br />
+								<i>{suggestion.name} </i>
+								<br />
+								<small>{suggestion.address + ' ' + suggestion.pincode}</small>
+							</li>
+						);
+					})}
+				</ul>
+			);
+		} else {
+			DataListView = (
+				<ul className="list-group">
+					<li className="list-group-item" key={'no-data'}>
+						No User Found
+					</li>
+				</ul>
+			);
 		}
+	}
 
-		return (
-			<React.Fragment>
-				<div className="container">
-					<div className="row">
-						<div className="form-group col-lg-4">
-							<div className="form-group">
-								<div className="form-group has-search">
-									<label className="control-label">Filter</label>
-									<span className="fa fa-search form-control-feedback"></span>
-									<input
-										type="search"
-										className="form-control"
-										onChange={onChange}
-										onKeyDown={onKeyDown}
-										value={userInput}
-										placeholder="Search users by ID,address,nam..."
-									/>
-								</div>
-							</div>
-							{DataListView}
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="form-group col-lg-4">
+					<div className="form-group">
+						<div className="form-group has-search">
+							<label className="control-label">Filter</label>
+							<span className="fa fa-search form-control-feedback"></span>
+							<input
+								type="search"
+								className="form-control"
+								onChange={onChange}
+								onKeyDown={onKeyDown}
+								value={userInput}
+								placeholder="Search users by ID,address,nam..."
+							/>
 						</div>
 					</div>
+					{DataListView}
 				</div>
-			</React.Fragment>
-		);
-	}
-}
-
+			</div>
+		</div>
+	);
+};
 export default Filter;
